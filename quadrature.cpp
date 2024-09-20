adc68537e7b8075bfccee3243415feff1746a27b
Fluntin <villim@kth.se>
2024-09-20 20:31:48 +0200

    123

--- quadrature.cpp
+++ quadrature.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <functional>
 #include <cmath>
 #include <stdexcept>
-#include "quadrature.h"
+#include "quadrature.h"  // Include the corresponding header
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -12,16 +11,12 @@ using namespace std;
 
 template <typename Func>
 inline double computeSimpson(Func f, int* evalCounter, double a, double b) {
-    
     *evalCounter += 4;
     return ((b - a) / 6) * (f(a) + 4 * f((a + b) / 2) + f(b));
 }
 
-
 template <typename Func>
-
-double simpsonAdaptive(Func f, int* evalCounter, double a, double b, double tol, int depth = 0, int maxDepth = 1000) {
-
+double simpsonAdaptive(Func f, int* evalCounter, double a, double b, double tol, int depth, int maxDepth) {
     if (tol <= 0) {
         throw underflow_error("Tolerance too small: " + to_string(tol));
     }
@@ -38,7 +33,6 @@ double simpsonAdaptive(Func f, int* evalCounter, double a, double b, double tol,
 
     if (error < 15 * tol) {
         return I2;
-
     } else {
         return simpsonAdaptive(f, evalCounter, a, mid, tol / 2, depth + 1, maxDepth) +
                simpsonAdaptive(f, evalCounter, mid, b, tol / 2, depth + 1, maxDepth);
--- unitTests.cpp
+++ unitTests.cpp
@@ -2,24 +2,19 @@
 #include <stdexcept>
 #include <cmath>
 #include "utest.h"
-#include "quadrature.h"
-
+#include "quadrature.h" 
 using namespace std;
 
-// Unit Test for checking if the integral of f(x) = x from 0 to 1 equals 1/2
 UTEST(simpson, check_integral_value) {
     int evalCounter = 0;
     auto identityFunction = [](double x) {
         return x;
     };
 
-    // Using a very small tolerance for high accuracy
     double integralValue = simpsonAdaptive(identityFunction, &evalCounter, 0, 1, 1e-9);
     double expectedValue = 0.5;
 
-    // Assert that the computed value matches the expected value (1/2)
     ASSERT_NEAR(integralValue, expectedValue, 1e-9);
 }
 
-// This runs the unit test
 UTEST_MAIN();
